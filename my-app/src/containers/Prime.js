81edaf3d74ea7df90cda371531841c6004b6786f
gilian <gilian@mailfence.com>
2018-05-31 14:13:14 +0200

    purge app , add containers

---
+++ my-app/src/containers/Prime.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Prime extends Component {
+    render() {
+        return (
+            <div>
+                <p>Prime</p>
+            </div>
+        )
+    }
+}
