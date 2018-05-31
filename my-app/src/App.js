519aeb389dea00cbf6582c86f97374d4ba53f1cf
gilian <gilian@mailfence.com>
2018-05-31 14:31:35 +0200

    add page root

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './containers/Home'
+import Graph from './containers/Graph'
+import Music from './containers/Music'
+import Prime from './containers/Prime'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>hello</p>
+        <BrowserRouter>
+          <div>
+            <Route path="/" component={Home} />
+            <Route path="/Music" component={Music} />
+            <Route path="/Graph" component={Graph} />
+            <Route path="/Prime" component={Prime} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- my-app/src/containers/Home.js
+++ my-app/src/containers/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <p>home</p>
+                <ul>
+                    <li><Link to="/Music">Music</Link></li>
+                    <li><Link to="/Prime">Prime</Link></li>
+                    <li><Link to="/Graph">Graph</Link></li>
+                </ul>
             </div>
         )
     }
