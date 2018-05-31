7b0bbf3db60efdc9cb7755a6897cd6d9e618b89b
gilian <gilian@mailfence.com>
2018-05-31 14:50:49 +0200

    prime, modification handleChange, add handleClick

--- my-app/src/containers/Prime.js
+++ my-app/src/containers/Prime.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
 export default class Prime extends Component {
-    handleChange(e) {
-        console.log(e)
+    constructor() {
+        super()
+        this.state = {
+            input: 0,
+            result: 0
+        }
+    }
+    //arrow function for binding
+    handleChange = (e) => {
+        this.setState({ input: e.target.value })
+        console.log(this.state.input)
+    }
+    handleClick() {
+        console.log("test")
     }
     render() {
         return (
             <div>
                 <p>Selection du n-ième chiffre premier</p>
-                <input onChange={this.handleChange} />
-                <button> Valider</button>
+                <input type="number" onChange={this.handleChange} />
+                <button onClick={this.handleClick}> Valider</button>
                 <p>result</p>
             </div>
         )
