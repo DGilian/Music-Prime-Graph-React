06aefeb9b010a949b507872a48aa4384e6cb6cbb
gilian <gilian@mailfence.com>
2018-05-31 14:41:02 +0200

    prime add onchange input

--- my-app/src/containers/Prime.js
+++ my-app/src/containers/Prime.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
 export default class Prime extends Component {
+    handleChange(e) {
+        console.log(e)
+    }
     render() {
         return (
             <div>
                 <p>Selection du n-ième chiffre premier</p>
-                <input />
+                <input onChange={this.handleChange} />
                 <button> Valider</button>
                 <p>result</p>
             </div>
