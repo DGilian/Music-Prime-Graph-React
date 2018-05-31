81937e59b294fd56dc472165d20aa9928b27e3cb
gilian <gilian@mailfence.com>
2018-05-31 14:37:10 +0200

    modif prime

--- my-app/src/containers/Prime.js
+++ my-app/src/containers/Prime.js
@@ -4,7 +4,10 @@ export default class Prime extends Component {
     render() {
         return (
             <div>
-                <p>Prime</p>
+                <p>Selection du n-ième chiffre premier</p>
+                <input />
+                <button> Valider</button>
+                <p>result</p>
             </div>
         )
     }
